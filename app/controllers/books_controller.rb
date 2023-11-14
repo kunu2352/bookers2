2c4c5dd57fce2be3ada74a7785f3f4651f8a3539
EC2 Default User <ec2-user@ip-172-31-34-181.ap-northeast-1.compute.internal>
2023-11-14 13:34:52 +0000

    課題粗方機能完成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -15,6 +15,8 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
+    
   end
 
   def show
@@ -23,10 +25,21 @@ class BooksController < ApplicationController
   
   def update
     @book = Book.find(params[:id])
-    
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
   
   def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   get 'homes/about' => 'homes#about', as: :about
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [:show, :edit, :index]
+  resources :users, only: [:show, :edit, :index, :update]
 end
